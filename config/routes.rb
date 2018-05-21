2c3a347e5c83988b6e74f139e4fb665121e51ea4
Viktharien <viktharien@zoho.com>
2018-05-21 21:25:54 +0000

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'categories#index'
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'categories#index'
+  
+    resources :categories do
+      resources :words
+      resources :phrases
+      
+    end
 end
