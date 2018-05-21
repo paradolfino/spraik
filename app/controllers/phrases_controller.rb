33cb5f9c7c45866e4b3a431f44eeb242165289ce
Viktharien <viktharien@zoho.com>
2018-05-21 20:29:36 +0000

    create controllers for categories, words, and phrases with actions

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,2 +1,49 @@
 class PhrasesController < ApplicationController
+    
+    def index
+       @phrase = Phrase.all 
+    end
+    
+    def show
+        
+    end
+    
+    def new
+       @phrase = Phrase.new 
+    end
+    
+    def create
+       @phrase = Phrase.new(phrase_params) 
+       if @phrase.save
+          redirect_to phrases_path 
+       else
+           render 'new'
+       end
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        if @phrase.save
+          redirect_to @phrase 
+        else
+           render 'edit'
+        end
+    end
+    
+    def destroy
+        @phrase.destroy
+    end
+    
+    private
+    
+        def set_phrase
+           @phrase = Phrase.find(params[:id]) 
+        end
+        
+        def phrase_params
+           params.require(:phrase).permit(:title) 
+        end
 end
