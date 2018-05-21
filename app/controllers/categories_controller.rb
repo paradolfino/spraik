6d9ecb571a92ff533319222623dae3b52664877e
Viktharien <viktharien@zoho.com>
2018-05-21 20:24:33 +0000

    create controllers for categories, words, and phrases with actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,16 @@ class CategoriesController < ApplicationController
        @category = Category.all 
     end
     
+    def show
+        
+    end
+    
+    def new
+       @category = Category.new 
+    end
+    
+    def create
+       @category = Category.new(category_params) 
+    end
+    
 end
