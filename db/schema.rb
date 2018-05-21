92b3eac82d3cff393daa76a79fa94687b65b49a8
Viktharien <viktharien@zoho.com>
2018-05-21 20:16:31 +0000

    generate models for category, word, phrase

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_21_201622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phrases", force: :cascade do |t|
+    t.text "eng"
+    t.text "nor"
+    t.bigint "word_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_phrases_on_category_id"
+    t.index ["word_id"], name: "index_phrases_on_word_id"
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "eng_si"
+    t.string "nor_si"
+    t.string "eng_pl"
+    t.string "nor_pl"
+    t.string "eng_def_si"
+    t.string "nor_def_si"
+    t.string "eng_def_pl"
+    t.string "nor_def_pl"
+    t.string "eng_past"
+    t.string "nor_past"
+    t.string "eng_fut"
+    t.string "nor_fut"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_words_on_category_id"
+  end
+
+  add_foreign_key "phrases", "categories"
+  add_foreign_key "phrases", "words"
+  add_foreign_key "words", "categories"
 end
