e675e6cc109af9aef8fed9ab340c6955f3a3ec9d
Viktharien <viktharien@zoho.com>
2018-05-21 20:33:29 +0000

    add columns to Word model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_201622) do
+ActiveRecord::Schema.define(version: 2018_05_21_203304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2018_05_21_201622) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "eng_pre"
+    t.string "nor_pre"
     t.index ["category_id"], name: "index_words_on_category_id"
   end
 
