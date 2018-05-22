6df57fe431caf82221c99f0873ab8dfd6f227c77
Viktharien <viktharien@zoho.com>
2018-05-22 16:58:19 +0000

    set up phrases, fix any issues with words

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'categories#index'
   
-    get 'category/:id/words/new' => 'categories#new_word', 'new_category_word'
+    get 'category/:id/words/new' => 'categories#new_word', as: 'new_category_word'
     post 'category/:id/words/new' => 'categories#create_word'
     resources :categories
     resources :words
