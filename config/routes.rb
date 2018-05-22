e7f745b2e60265282d6654f5a5ff11432111f6e7
Viktharien <viktharien@zoho.com>
2018-05-22 17:23:09 +0000

    set up phrases, fix any issues with words

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     root 'categories#index'
   
     get 'category/:id/words/new' => 'categories#new_cat_word', as: 'new_cat_word'
-    post 'category/:id/words/new' => 'categories#create_caty_word'
+    post 'category/:id/words/new' => 'categories#create_cat_word'
     resources :categories
     resources :words
     resources :phrases
