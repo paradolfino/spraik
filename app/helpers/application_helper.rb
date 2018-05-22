4afd493ff5c6886cb47e2b199ca8f6ca0ae94a76
Viktharien <viktharien@zoho.com>
2018-05-22 19:45:20 +0000

    set up word show with view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
     
     def word_info(word, label)
-        if word != ""
+        if word != "" || word != nil
            return "<span class='label'>#{label}:</span> #{word}".html_safe
         end
     end
     
+    def h1_for_obj(obj, label)
+        if obj != "" || obj != nil
+            
+        end
 end
